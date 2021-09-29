9251839582d01d15bff50959a32d8efa1cffc928
Ricardo Carteri Uehlein <ricardo_carteri@hotmail.com>
2021-09-29 14:29:18 -0300

    Upload configuração

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,15 +3,15 @@ require 'capybara/cucumber'
 require 'site_prism'
 require 'faker'
 require 'cpf_faker'
- 
+
 Capybara.register_driver :site_prism do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
 Capybara.configure do |config|
-  config.run_server = false
-  Capybara.default_driver = :site_prism
-  Capybara.page.driver.browser.manage.window.maximize
-  config.default_max_wait_time = 10
-  config.app_host = 'http://www.lourencodemonaco.com.br/qarentena'
+    config.run_server = false
+    Capybara.default_driver = :site_prism
+    Capybara.page.driver.browser.manage.window.maximize
+    config.default_max_wait_time = 10
+    config.app_host = 'http://www.lourencodemonaco.com.br/qarentena'
 end
